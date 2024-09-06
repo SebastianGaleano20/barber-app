ba320b13e23f6cab51d17deb85a350296bb52f19
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-09-06 13:23:46 +0000

    serices configurated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 import Header from "@/app/ui/dashboard/Header"
-
+import Services from "./ui/dashboard/Services"
 export default function Home() {
   return (
     <>
     <main className="lg:flex flex-col items-center text-center">
     <Header />
+    <Services />
     </main>
     </>
   )
--- src/app/ui/dashboard/Services.jsx
+++ src/app/ui/dashboard/Services.jsx
@@ -1,20 +1,35 @@
+const SERVICES = [
+  {
+    id: 1,
+    imageURL: '/corte3.jpeg',
+    name: 'CORTES UNISEX / NIÑOS',
+    description: 'Clásico o moderno. Marcamos tendencia combinando máquinas y tijeras.'
+  },
+  {
+    id: 2,
+    name: 'CORTE + BARBA',
+    imageURL: 'corte1.jpeg',
+    description: 'Clásico o moderno más afeitada tradicional o recorte de barba.'
+  },
+  {
+    id: 3,
+    imageURL: '/corte2.jpeg',
+    name: 'COLORES',
+    description: 'Proceso de coloracion para cabello. Consultar gama de colores'
+  }]
+
 export default function Services() {
   return (
-    <section className="flex flex-col justify-center content-between items-center text-center p-5">
-      <article>
-        <h2>CORTES UNISEX / NIÑOS</h2>
-        <p>Clásico o moderno. Marcamos tendencia combinando máquinas y tijeras.</p>
-      </article>
-      <article>
-        <h2>BARBA</h2>
-        <p>Afeitada tradicional o recorte de barba.</p>
-      </article>
-      <article>
-        <h2>
-          COLORES
-        </h2>
-        <p>Proceso de coloracion para cabello. Consultar gama de colores</p>
-      </article>
-    </section>
+    < section className = "flex flex-wrap justify-center gap-4 m-3" >
+   {
+      SERVICES.map(service => (
+        <article className="max-w-md bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={service.id}>
+          <img className="rounded-t-lg" src={service.imageURL} alt="img" />
+          <h2 className="text-2xl p-5">{service.name}</h2>
+          <p className="mb-3 text-2lg font-normal text-gray-700 dark:text-gray-400">{service.description}</p>
+        </article>
+      ))
+    }
+    </section >
   )
 }
