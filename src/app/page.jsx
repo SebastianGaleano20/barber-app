e8bb9dbe736f8a8d5ae1e43d319389b65bfd0c19
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-09-06 14:02:54 +0000

    footer actualizated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,15 @@
 import Header from "@/app/ui/dashboard/Header"
 import Services from "./ui/dashboard/Services"
+import Footer from "./ui/dashboard/Footer"
+
 export default function Home() {
   return (
     <>
-    <main className="text-center">
-    <Header />
-    <Services />
-    </main>
+      <main className="text-center">
+        <Header />
+        <Services />
+        <Footer />
+      </main>
     </>
   )
 }
