8916132572bc507f515e3901553399a4ca48c2ec
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-09-20 15:04:49 +0000

    ImageGallery created

--- src/app/ui/dashboard/ImageGallery.jsx
+++ src/app/ui/dashboard/ImageGallery.jsx
@@ -2,47 +2,55 @@ import Image from "next/image"
 
 export default function ImageGallery() {
   return (
-    <section className="carousel carousel-center bg-neutral rounded-box max-w-md space-x-4 p-4">
-      <article className="carousel-item">
-        <img
-          src="/logo.jpeg"
-          width={500}
-          height={500}
-          alt="Picture of the author"
-          className="rounded-box" />
-      </article>
-      <article className="carousel-item">
-        <img
-          src="/corte1.jpeg"
-          width={500}
-          height={500}
-          alt="Picture of the author"
-          className="rounded-box" />
-      </article>
-      <article className="carousel-item">
-        <img
-          src="/corte2.jpeg"
-          width={500}
-          height={500}
-          alt="Picture of the author"
-          className="rounded-box" />
-      </article>
-      <article className="carousel-item">
-        <img
-          src="/corte3.jpeg"
-          width={500}
-          height={500}
-          alt="Picture of the author"
-          className="rounded-box" />
-      </article>
-      <article className="carousel-item">
-        <img
-          src="/logo.jpeg"
-          width={500}
-          height={500}
-          alt="Picture of the author"
-          className="rounded-box" />
-      </article>
-    </section>
+    <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+  <Image 
+width={420} 
+height={500} 
+src="/corte1.jpeg" 
+alt="Pizza" 
+className="w-full"/>
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide4" className="btn btn-circle">❮</a>
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide2" className="carousel-item relative w-full">
+  <Image 
+width={420} 
+height={500} 
+src="/logo.jpeg" 
+alt="Pizza" 
+className="w-full"/>
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide1" className="btn btn-circle">❮</a>
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide3" className="carousel-item relative w-full">
+  <Image 
+width={420} 
+height={500} 
+src="/corte3.jpeg" 
+alt="Pizza" 
+className="w-full"/>
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide2" className="btn btn-circle">❮</a>
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide4" className="carousel-item relative w-full">
+  <Image 
+width={420} 
+height={500} 
+src="/corte2.jpeg" 
+alt="Pizza" 
+className="w-full"/>
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide3" className="btn btn-circle">❮</a>
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
   )
 }
