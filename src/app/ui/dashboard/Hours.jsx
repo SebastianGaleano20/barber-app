093b38d70fe7fe61f3a43ac73ac4748ec14f19cc
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-09-27 14:56:45 +0000

    Hours Component update

--- src/app/ui/dashboard/Hours.jsx
+++ src/app/ui/dashboard/Hours.jsx
@@ -1,131 +1,133 @@
 export default function Hours() {
     return (
-        <section className="card bg-slate-600 ring-2 ring-slate-800 p-6 rounded-lg w-11/12">
-                <h2 className="text-3xl font-bold text-center mb-4">Horarios</h2>
-                <ul className="timeline timeline-vertical lg:timeline-horizontal">
-                    <li>
-                        <div className="timeline-start">Lunes</div>
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-end timeline-box">Cerrado</div>
-                        <hr />
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-start">Martes</div>
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-end timeline-box">12hs a 19hs</div>
-                        <hr />
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-start">Miercoles</div>
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-end timeline-box">12hs a 19hs</div>
-                        <hr />
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-start">Jueves</div>
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-end timeline-box">12hs a 19hs</div>
-                        <hr />
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-start">Viernes</div>
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-end timeline-box">11hs a 20hs</div>
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-start">Sabado</div>
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-end timeline-box">11hs a 20hs</div>
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-start">Domingo</div>
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-end timeline-box">Cerrado</div>
-                    </li>
-                </ul>
+        <section className="lg:text-2xl lg:flex lg:justify-center lg:items-center lg:flex-col lg:w-full lg:h-full lg:gap-4 card bg-slate-600 ring-2 ring-slate-800 p-6 rounded-lg w-11/12">
+            <h2 className="text-3xl font-bold text-center mb-4">Horarios</h2>
+            <ul className="timeline timeline-vertical lg:timeline-horizontal">
+                <li>
+                    <div className="timeline-start">Lunes</div>
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5">
+                            <path
+                                fillRule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div className="timeline-end timeline-box">Cerrado</div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-start">Martes</div>
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5">
+                            <path
+                                fillRule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div className="timeline-end timeline-box">12hs a 19hs</div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-start">Miercoles</div>
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5">
+                            <path
+                                fillRule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div className="timeline-end timeline-box">12hs a 19hs</div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-start">Jueves</div>
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5">
+                            <path
+                                fillRule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div className="timeline-end timeline-box">12hs a 19hs</div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-start">Viernes</div>
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5">
+                            <path
+                                fillRule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div className="timeline-end timeline-box">11hs a 20hs</div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-start">Sabado</div>
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5">
+                            <path
+                                fillRule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div className="timeline-end timeline-box">11hs a 20hs</div>
+                    <hr />
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-start">Domingo</div>
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5">
+                            <path
+                                fillRule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div className="timeline-end timeline-box">Cerrado</div>
+                </li>
+            </ul>
         </section>
     )
 }
