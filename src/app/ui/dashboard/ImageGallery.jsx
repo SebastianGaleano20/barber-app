507077dd64edf23b355a114b69007e93242517ca
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-09-27 13:44:58 +0000

    Image gallery logic update

--- src/app/ui/dashboard/ImageGallery.jsx
+++ src/app/ui/dashboard/ImageGallery.jsx
@@ -1,11 +1,18 @@
+'use client'
 import { Images } from "../../images.js"
+import { useState, useEffect } from "react";
 
 export default function ImageGallery() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    setImages(Images);
+  }, []);
   return (
     <>
       <section className="carousel w-full lg:hidden">
         {
-          Images.map((image, index) => (
+          images.map((image, index) => (
             <article key={index} id={`slide${index + 1}`} className="carousel-item relative w-full">
               <img
                 src={image.image}
@@ -13,57 +20,38 @@ export default function ImageGallery() {
                 alt={image.alt}
               />
               <article className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a 
-                href={`#slide${index === 0 ? Images.length : index}`} 
-                className="btn btn-circle">❮</a>
-                <a 
-                 href={`#slide${index === Images.length - 1 ? 1 : index + 2}`} 
-                className="btn btn-circle">❯</a>
+                <a
+                  href={`#slide${index === 0 ? Images.length : index}`}
+                  className="btn btn-circle">❮</a>
+                <a
+                  href={`#slide${index === Images.length - 1 ? 1 : index + 2}`}
+                  className="btn btn-circle">❯</a>
               </article>
             </article >
           ))
         }
       </section >
 
-
-      <div className="carousel lg:flex hidden overflow-hidden">
-        <div className="carousel-item">
-          <img
-            src="/corte6.jpeg"
-            className="max-w-md"
-            alt="img" />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="/corte7.jpeg"
-            className="max-w-md"
-            alt="img" />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="/corte8.jpeg"
-            className="max-w-md"
-            alt="img" />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="/corte9.jpeg"
-            className="max-w-md"
-            alt="img" />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="/corte2.jpeg"
-            className="max-w-md"
-            alt="img" />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="/corte1.jpeg"
-            className="max-w-md"
-            alt="img" />
-        </div>
-      </div>
+      <section className="carousel carousel-center rounded-box lg:flex hidden overflow-hidden">
+        <article className="carousel-item">
+          <img src="/corte9.jpeg" className="w-96" alt="Corte de pelo" />
+        </article>
+        <article className="carousel-item">
+          <img src="/corte7.jpeg" className="w-96" alt="Corte de pelo" />
+        </article>
+        <article className="carousel-item">
+          <img src="/corte3.jpeg" className="w-96" alt="Corte de pelo" />
+        </article>
+        <article className="carousel-item">
+          <img src="/corte4.jpeg" className="w-96" alt="Corte de pelo" />
+        </article>
+        <article className="carousel-item">
+          <img src="/corte5.jpeg" className="w-96" alt="Corte de pelo" />
+        </article>
+        <article className="carousel-item">
+          <img src="/corte6.jpeg" className="w-96" alt="Corte de pelo" />
+        </article>
+      </section>
     </>
   )
 }
