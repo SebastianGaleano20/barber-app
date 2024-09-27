2e06afbc4e95149e01a548281b212c6ecaa12071
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-09-27 14:44:18 +0000

    Header component update

--- src/app/ui/dashboard/Header.jsx
+++ src/app/ui/dashboard/Header.jsx
@@ -3,14 +3,16 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-      <section 
-      className="lg:w-full lg:h-full lg:flex lg:flex-col lg:justify-center lg:items-center lg:image-full grid p-4 text-center h-5/6 w-full">
-          <figure>
-            <img
-              src="/logo.jpeg"
-              alt="logo" />
-          </figure>
-        <p className="lg:text-2xl lg: text-lg font-bold">Barbería familiar ubicada en el barrio de Parque Patricios, desde el año 2020 brindando la mejor experiencia.</p>
-      </section>
+    <section
+      className="bg-[url(/fondo2.png)] bg-center bg-no-repeat bg-cover lg:w-full lg:h-full lg:flex lg:flex-col lg:justify-center lg:items-center lg:image-full grid text-center h-5/6 w-full">
+      <figure>
+        <img
+          src="/logo.jpeg"
+          alt="logo" />
+      </figure>
+      <div className='bg-gray-500 bg-opacity-10 lg:bg-opacity-5 w-full m-0'>
+        <h2 className="lg:text-3xl text-shadow text-black text-2xl font-bold">Barbería familiar ubicada en el barrio de Parque Patricios, desde el año 2020 brindando la mejor experiencia.</h2>
+      </div>
+    </section>
   )
 }
