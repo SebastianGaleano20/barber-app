164be5599d3d4d1a5e5f76c93c3ccc00cd2dbd72
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-09-29 16:46:00 +0000

    fix update

--- src/app/ui/dashboard/Barbers.jsx
+++ src/app/ui/dashboard/Barbers.jsx
@@ -18,8 +18,11 @@ export default function Barbers() {
                     <h2 className="text-center font-bold text-3xl">Jony</h2>
                     <p>Barber profesional con experiencia de 4 años junto al equipo de Barrio Fino.</p>
                     <div className="card-actions justify-between mt-2">
-                        <button className="btn btn-outline btn-error"><FaInstagram /><a target="_blank" href="https://www.instagram.com/jonimasna?igsh=MTR1ZW5rMmY3MDhvaQ==">Ver Perfil</a></button>
-                        <button className="btn btn-outline btn-accent"><FaWhatsapp /><a target="_blank" href="https://wa.me/+5491131675543?text=Hola%20quiero%20solicitar%20un%20turno%20porfavor"></a>Solicitar turno</button>
+                        <button className="btn btn-outline btn-error">
+                            <FaInstagram />
+                            <a target="_blank" href="https://www.instagram.com/jonimasna?igsh=MTR1ZW5rMmY3MDhvaQ==">Ver Perfil</a>
+                        </button>
+                        <button className="btn btn-outline btn-accent"><FaWhatsapp /><a target="_blank" href="https://wa.me/+5491131675543?text=Hola%20quiero%20solicitar%20un%20turno%20porfavor">Solicitar turno</a></button>
                     </div>
                 </article>
             </article>
@@ -36,8 +39,14 @@ export default function Barbers() {
                     <h2 className="text-center font-bold text-3xl">Juan Ignacio</h2>
                     <p>Barber profesional - Fundador de Barrio Fino.</p>
                     <div className="card-actions justify-between mt-2">
-                        <button className="btn btn-outline btn-error"><FaInstagram /><a target="_blank" href="https://www.instagram.com/san_cristobal13?igsh=YnhtdGdwNXZmNGM2">Ver Perfil</a></button>
-                        <button className="btn btn-outline btn-accent"><FaWhatsapp /><a target="_blank" href="https://wa.me/+5491123835826?text=Hola%20quiero%20solicitar%20un%20turno%20porfavor"></a>Solicitar turno</button>
+                        <button className="btn btn-outline btn-error">
+                            <FaInstagram />
+                            <a target="_blank" href="https://www.instagram.com/san_cristobal13?igsh=YnhtdGdwNXZmNGM2">Ver Perfil</a>
+                        </button>
+                        <button className="btn btn-outline btn-accent">
+                            <FaWhatsapp />
+                            <a target="_blank" href="https://wa.me/+5491123835826?text=Hola%20quiero%20solicitar%20un%20turno%20porfavor">Solicitar turno</a>
+                        </button>
                     </div>
                 </article>
             </article>
@@ -54,8 +63,14 @@ export default function Barbers() {
                     <h2 className="text-center font-bold text-3xl">Juan Manuel</h2>
                     <p>Barber profesional - Fundador de Barrio Fino.</p>
                     <div className="card-actions justify-between mt-2">
-                        <button className="btn btn-outline btn-error"><FaInstagram /><a target="_blank" href="https://www.instagram.com/lemosjuan23?igsh=Nnl2Y3B1OXphb2lr">Ver Perfil</a></button>
-                        <button className="btn btn-outline btn-accent"><FaWhatsapp /><a target="_blank" href="https://wa.me/+5491141949307?text=Hola%20quiero%20solicitar%20un%20turno%20porfavor">Solicitar turno</a></button>
+                        <button className="btn btn-outline btn-error">
+                            <FaInstagram />
+                            <a target="_blank" href="https://www.instagram.com/lemosjuan23?igsh=Nnl2Y3B1OXphb2lr">Ver Perfil</a>
+                            </button>
+                        <button className="btn btn-outline btn-accent">
+                            <FaWhatsapp />
+                            <a target="_blank" href="https://wa.me/+5491141949307?text=Hola%20quiero%20solicitar%20un%20turno%20porfavor">Solicitar turno</a>
+                        </button>
                     </div>
                 </article>
             </article>
