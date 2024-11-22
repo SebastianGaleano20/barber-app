d4f2af86ce1978faae06fa3389b703cb850119e0
Sebastian Galeano <sebascarp9@gmail.com>
2024-11-22 10:50:44 -0300

    Location component Darkmode added

--- src/app/ui/dashboard/Location.jsx
+++ src/app/ui/dashboard/Location.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Location() {
     return (
-        <section className="card bg-base-100 max-w-md md:h-96 lg:h-auto lg:row-span-2 m-4 overflow-hidden ">
+        <section className="card bg-base-100 max-w-md md:h-96 lg:h-auto lg:row-span-2 m-4 overflow-hidden bg-white dark:bg-gray-900">
                 <figure>
                     <Image
                         width={500}
@@ -12,8 +12,8 @@ export default function Location() {
                 </figure>
             <article className="flex items-start text-white text-center">
                 <article className="card-body">
-                    <h2 className="card-title">Ubicación del local</h2>
-                    <p className="text-start">Alberti 2031 - Parque Patricios, Buenos Aires</p>
+                    <h2 className="card-title text-gray-800 dark:text-gray-100">Ubicación del local</h2>
+                    <p className="text-start text-gray-800 dark:text-gray-100">Alberti 2031 - Parque Patricios, Buenos Aires</p>
                     <div className="card-actions">
                         <button className="btn btn-primary"><a target="_blank" href="https://maps.app.goo.gl/CyysiyPupd8qPUur8">Ver mapa</a></button>
                     </div>
